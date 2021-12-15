46538a250d86b663d517d40f5dff8c8e8d1fac1e
Juan Fermin <juanfob.dev@gmail.com>
2021-12-15 20:50:48 +0100

    Código completo

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { environment } from '../environments/environment';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideStorage, getStorage } from '@angular/fire/storage';
+
+@NgModule({
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+    provideAuth(() => getAuth()),
+    provideStorage(() => getStorage())
+  ],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAQ7ZJL-fGwaBaWSSMQOaFFfS8ee5F1OL8',
+    authDomain: 'ionicacademy-chat-course-21a4f.firebaseapp.com',
+    projectId: 'ionicacademy-chat-course-21a4f',
+    storageBucket: 'ionicacademy-chat-course-21a4f.appspot.com',
+    messagingSenderId: '714039969946',
+    appId: '1:714039969946:web:4bb734d6c0f0e051027723'
+  }
+};
